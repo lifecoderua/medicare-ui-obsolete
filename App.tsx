034a07e931eeae4bd1ff4c85fedcbd0be73d49bf
lifecoder <lifecoder.ua@gmail.com>
2020-01-04 13:10:33 +0200

    screen component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WelcomeScreen } from './src/screens/welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>2. Open up App.tsx to start working on your app!</Text>
+      <WelcomeScreen/>
     </View>
   );
 }
