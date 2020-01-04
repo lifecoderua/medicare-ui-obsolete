ff221e24aa356d85c4578bb4a54e93b0023aceb8
lifecoder <lifecoder.ua@gmail.com>
2020-01-04 21:26:19 +0200

    ios hack for web-based firebase sdk

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+// platform setup
+import './platform/firebase/firebase-preset';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WelcomeScreen } from './src/screens/welcome';
