75430055563f7cace7dc5b9f75fb01b9cb8bad5a
lifecoder <lifecoder.ua@gmail.com>
2020-01-04 18:34:11 +0200

    firebase web sdk added

--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
+import * as firebase from 'firebase';
 
 interface State {
   email: string,
@@ -13,6 +14,45 @@ export class WelcomeScreen extends Component<{}, State> {
       email: '',
       password: '',
     };
+
+    this.dbRead();
+  }
+
+  async dbRead() {
+    // Initialize Firebase
+    const firebaseConfig = {
+      apiKey: "AIzaSyAYhQBOC2BdyH5KN29cfec8H1DFG_3DbkM",
+      authDomain: "medicare-4ccfe.firebaseapp.com",
+      databaseURL: "https://medicare-4ccfe.firebaseio.com",
+      projectId: "medicare-4ccfe",
+      storageBucket: "medicare-4ccfe.appspot.com",
+      messagingSenderId: "793358878810",
+      appId: "1:793358878810:web:3747de3a7947eaadc83063",
+      measurementId: "G-DYLBCH4VKE"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+    const db = firebase.firestore();
+
+    // db.collection("users").add({
+    //   first: "Ada",
+    //   last: "Lovelace",
+    //   born: 1815
+    // })
+    // .then(function(docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch(function(error) {
+    //     console.error("Error adding document: ", error);
+    // });
+
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        });
+    });
   }
 
   render() {
