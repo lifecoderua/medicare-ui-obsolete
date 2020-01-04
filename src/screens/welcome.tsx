4ae2510852e1ca6cc0b939381dca50446824952a
lifecoder <lifecoder.ua@gmail.com>
2020-01-04 15:34:24 +0200

    input fields

--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
 
-export class WelcomeScreen extends Component {
+interface State {
+  email: string,
+  password: string,
+}
+
+export class WelcomeScreen extends Component<{}, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>I am the Welcome screen</Text>
-        <Text>Hello <strong>John Dou</strong></Text>
+        <Text>Hello <strong>John Doe</strong></Text>
+        <TextInput
+          placeholder="Email"
+          onChangeText={(email) => this.setState({email})}
+          value={this.state.email}
+          />
+        <TextInput
+          placeholder="Password"
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+          />
+        <Text>{this.state.email} | {this.state.password}</Text>
+        <Button
+          title="Log In"
+          onPress={() => console.log('Submit:', this.state)}
+        ></Button>
+        <TouchableOpacity
+          onPress={() => console.log('Submit:', this.state)}>
+          <Text>Press me for submit</Text>
+        </TouchableOpacity>
       </View>
     );
   }
