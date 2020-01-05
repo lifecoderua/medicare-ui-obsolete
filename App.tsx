2adbf799dffa002dd44b3beef29e30789f2c0fbf
lifecoder <lifecoder.ua@gmail.com>
2020-01-05 21:27:03 +0200

    single page navigation

--- App.tsx
+++ App.tsx
@@ -5,21 +5,32 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WelcomeScreen } from './src/screens/welcome';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>2. Open up App.tsx to start working on your app!</Text>
-      <WelcomeScreen/>
-    </View>
-  );
-}
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: WelcomeScreen,
   },
 });
+
+export default createAppContainer(AppNavigator);
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <Text>2. Open up App.tsx to start working on your app!</Text>
+//       <WelcomeScreen/>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
