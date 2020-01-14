8b7783453417ed48ba2e99b229a5f8d96a51cc5a
lifecoder <lifecoder.ua@gmail.com>
2020-01-14 21:39:23 +0200

    store service cleanup

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -17,34 +17,8 @@ class Firebase {
   
   constructor() {
     firebase.initializeApp(firebaseConfig);
-
     this.db = firebase.firestore();
   }
-
-  
-
-    // try {
-    //   await db.enablePersistence();
-    //   this.setState({persistence: 'Offline support activated!'});
-    // } catch(err) {
-    //     if (err.code == 'failed-precondition') {
-    //         // Multiple tabs open, persistence can only be enabled
-    //         // in one tab at a a time.
-    //         // ...
-    //         this.setState({persistence: 'Another tab is open, please close one of the tabs!'})
-    //     } else if (err.code == 'unimplemented') {
-    //         // The current browser does not support all of the
-    //         // features required to enable persistence
-    //         // ...
-    //         this.setState({persistence: 'Offline support UNAVAILABLE!'})
-    //     }
-    // };
-
-    // await db.disableNetwork();
-    // console.log('network disabled');
-
-    // await db.enableNetwork();
-    // console.log('network enabled');
 }
 
 const store = new Firebase();
