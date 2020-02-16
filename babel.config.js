8d964719fb38abdaa320badafe4222083e43eb3a
lifecoder <lifecoder.ua@gmail.com>
2020-02-16 23:12:14 +0200

    magic for ios9 build; const don't convert into var

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    ignore: [
+      /\/core-js/,
+      /@babel\/runtime/,
+    ],
+    presets: [
+      'babel-preset-expo',
+      [
+        "@babel/preset-env",
+        {
+          "targets": {
+            "chrome": "58",
+            "ios": "9",
+          },
+          "include": [
+            "transform-block-scoping"
+          ],
+          useBuiltIns: 'usage',
+          corejs: 3,
+          forceAllTransforms: true,
+
+          debug: true,
+        }
+      ]
+    ],
+    "plugins": [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-transform-block-scoping"
+    ],
   };
 };
